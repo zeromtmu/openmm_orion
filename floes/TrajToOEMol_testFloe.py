3c7e79b92e50187928c8d22f4ff98840c43babb1
cbayly13 <bayly@eyesopen.com>
2018-06-11 16:31:15 -0600

    Introduced first working version of traj analysis floe and cube; this cube converts MD traj into multiconf protein and lig OEMols (one conf per traj frame).

---
+++ floes/TrajToOEMol_testFloe.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from floe.api import WorkFloe
+from cuberecord import DataSetWriterCube, DataSetReaderCube
+from TrjAnalysisCubes.TrajToOEMol import TrajToOEMolCube
+#
+job = WorkFloe("Testing TrajToOEMol")
+#
+job.description = """
+Testing Floe
+#
+Ex. python floes/up.py --ligands ligands.oeb
+--ofs-data_out prep.oeb
+#
+Parameters:
+-----------
+ligands (file): OEB file of the prepared ligands
+#
+Outputs:
+--------
+ofs: Output file
+"""
+#
+ifs = DataSetReaderCube("ifs")
+#
+ifs.promote_parameter("data_in", promoted_name="in", title="System Input OERecord", description="OERecord file name")
+#
+scube = TrajToOEMolCube("TrajToOEMolCube")
+#
+ofs = DataSetWriterCube('ofs', title='OFS-Success')
+ofs.promote_parameter("data_out", promoted_name="out", title="System Output OERecord", description="OERecord file name")
+#
+job.add_cubes(ifs, scube, ofs)
+#
+ifs.success.connect(scube.intake)
+scube.success.connect(ofs.intake)
+#
+if __name__ == "__main__":
+    job.run()
