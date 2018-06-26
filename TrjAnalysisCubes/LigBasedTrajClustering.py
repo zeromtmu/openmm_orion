d69888d7d7bd2aaf0c441fb7b23bc28b0b9c720d
cbayly13 <bayly@eyesopen.com>
2018-06-26 10:32:59 -0600

    Modified LigBasedTrajClustering.py to suppress RMSD heat map and save ClusterCounts.

--- TrjAnalysisCubes/LigBasedTrajClustering.py
+++ TrjAnalysisCubes/LigBasedTrajClustering.py
@@ -21,6 +21,9 @@ from openeye import oechem
 
 import TrjAnalysisCubes.Clustering_utils as clusutl
 
+# temporary
+import numpy as np
+
 def CheckAndGetValue( record, field, rType):
     if not record.has_value(OEField(field,rType)):
         #opt['Logger'].warn('Missing record field {}'.format( field))
@@ -94,32 +97,44 @@ class ClusterOETrajCube(ParallelMixin, OERecordComputeCube):
             clusResults = clusutl.ClusterLigTraj( ligTraj)
             opt['Logger'].info('{} plotting RMSD histogram'.format(system_title) )
             trajHistRMSD_svg = clusutl.ClusterLigTrajHistRMSD(clusResults)
-            opt['Logger'].info('{} plotting RMSD heat map'.format(system_title) )
-            trajHeatRMSD_png = clusutl.ClusterLigTrajHeatRMSD(clusResults)
+            # Heat map is large, time-consuming, and non-essential... put back if needed
+            #opt['Logger'].info('{} plotting RMSD heat map'.format(system_title) )
+            #trajHeatRMSD_png = clusutl.ClusterLigTrajHeatRMSD(clusResults)
             opt['Logger'].info('{} plotting cluster strip plot'.format(system_title) )
             trajClus_svg = clusutl.ClusterLigTrajClusPlot(clusResults)
 
             # Create new record with trajClus results
             opt['Logger'].info('{} writing trajClus OERecord'.format(system_title) )
-            ClusterMethod_field = OEField( 'ClusterMethod', Types.String)
-            HDBSCAN_alpha_field = OEField( 'HDBSCAN_alpha', Types.Float)
-            nFrames_field = OEField( 'nFrames', Types.Int)
-            nClusters_field = OEField( 'nClusters', Types.Int)
-            Clusters_field = OEField( 'Clusters', Types.IntVec)
-            HistSVG_field = OEField( 'HistSVG', Types.String, meta=OEFieldMeta().set_option(Meta.Hints.Image_SVG))
-            HeatPNG_field = OEField( 'HeatPNG', Types.Blob, meta=OEFieldMeta().set_option(Meta.Hints.Image_PNG))
-            ClusSVG_field = OEField( 'ClusSVG', Types.String, meta=OEFieldMeta().set_option(Meta.Hints.Image_SVG))
-
             trajClus = OERecord()
+            #
+            ClusterMethod_field = OEField( 'ClusterMethod', Types.String)
             trajClus.set_value( ClusterMethod_field, clusResults['ClusterMethod'])
+            #
+            HDBSCAN_alpha_field = OEField( 'HDBSCAN_alpha', Types.Float)
             trajClus.set_value( HDBSCAN_alpha_field, clusResults['HDBSCAN_alpha'])
+            #
+            nFrames_field = OEField( 'nFrames', Types.Int)
             trajClus.set_value( nFrames_field, clusResults['nFrames'])
+            #
+            nClusters_field = OEField( 'nClusters', Types.Int)
             trajClus.set_value( nClusters_field, clusResults['nClusters'])
+            #
+            clusterCounts_field = OEField( 'ClusterCounts', Types.IntVec)
+            trajClus.set_value( clusterCounts_field, clusResults['ClusterCounts'])
+            #
+            Clusters_field = OEField( 'Clusters', Types.IntVec)
             trajClus.set_value( Clusters_field, clusResults['ClusterVec'])
+            #
+            HistSVG_field = OEField( 'HistSVG', Types.String, meta=OEFieldMeta().set_option(Meta.Hints.Image_SVG))
             trajClus.set_value( HistSVG_field, trajHistRMSD_svg)
-            trajClus.set_value( HeatPNG_field, trajHeatRMSD_png)
+            #
+            ClusSVG_field = OEField( 'ClusSVG', Types.String, meta=OEFieldMeta().set_option(Meta.Hints.Image_SVG))
             trajClus.set_value( ClusSVG_field, trajClus_svg)
+            # Heat map is large, time-consuming, and non-essential... put back if needed
+            # HeatPNG_field = OEField( 'HeatPNG', Types.Blob, meta=OEFieldMeta().set_option(Meta.Hints.Image_PNG))
+            # trajClus.set_value( HeatPNG_field, trajHeatRMSD_png)
 
+            #
             record.set_value( OEField( 'TrajClus', Types.Record), trajClus)
             analysesDone.append( 'TrajClus')
             record.set_value( OEField( 'AnalysesDone', Types.StringVec), analysesDone)
