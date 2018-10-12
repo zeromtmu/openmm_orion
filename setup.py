664d7970182f422ee2fb240c8fe6772e39fb3eb9
cbayly13 <bayly@eyesopen.com>
2018-10-12 13:58:35 -0600

    Revved to version 0.7.11.2 after merge of gcalabro_data_record.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ def get_version():
 
 setup(
     name="MDOrion",
-    version='0.7.11.1',
+    version='0.7.11.2',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     author="Christopher Bayly, Gaetano Calabro, Nathan M. Lim, John Chodera, ",
