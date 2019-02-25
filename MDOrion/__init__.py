4c7b13a72d8a60523d20d0c396ddfed6ac9bcf2f
Gaetano Calabro <gcalabro@eyesopen.com>
2019-02-25 16:58:43 -0700

    change version to a stable release

--- MDOrion/__init__.py
+++ MDOrion/__init__.py
@@ -15,7 +15,7 @@
 # liable for any damages or liability in connection with the Sample Code
 # or its use.
 
-__version__ = '0.8.0b1'
+__version__ = '0.8.1'
 
 import ComplexPrepCubes
 import ForceFieldCubes
